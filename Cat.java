bac09c2e300ebb40b87767e813b51460db773715
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-06 22:25:27 -0700

    final

--- Cat.java
+++ Cat.java
@@ -1,69 +1,74 @@
-/**class to simulate a cat*/
+/** An abstract class to simulate a Cat */
 public abstract class Cat extends Player{
 
-	/**name of player's cat*/
+	/** Name of player's cat */
 	private String name;
-	/**level of cat's hunger*/
+	/** Level of cat's hunger */
 	private int hunger;
 	
-	/**intializes cat with given name
+	/** Intializes cat with given name
 	 * @param n name to be given to cat
 	 */
 	public Cat(String n){
-		name=n;
+		name = n;
 	}
 
-	/**Calls upon the cat's name
+	/** Gets the cat's name
 	 * @return cat's name
 	 */
 	public String getName(){
 		return name;
 	}
 
-	/**Measures the cat's hunger level
-	 *@return hunger level
+	/** Gets the cat's hunger level
+	 * @return hunger level
 	 */
 	int getHunger(){
 		return hunger;
 	}
 
-	/**Increment/decrement hunger level
-	 *@return hunger level
+	/** Increment/decrement hunger level
+	 * @return hunger level
 	 */
 	protected int incrementHunger(int val){
-		hunger+=val;
-		if(hunger<1){
-			hunger=1;
+		hunger += val;
+		if(hunger < 1){
+			hunger = 1;
 		}
-		else if(hunger>10){
-			hunger=10;
+		else if(hunger > 10){
+			hunger = 10;
 		}
 		return hunger;
 	}
 
-	/**Prints the cat's status
-	 *@return cat's status based on hunger level
+	/** Prints the cat's status
+	 * @return cat's status based on hunger level
 	 */
 	@Override
 	public String toString(){
-		if(hunger==10){
-			return getName()+" is currently full";
+		if(hunger == 10){
+			return getName() + " is currently full";
 		}
-		else if(hunger>5 && hunger<10){
-			return getName()+" is satisfied.";
+		else if(hunger > 5 && hunger < 10){
+			return getName() + " is satisfied.";
 		}
 		else{
-			return getName()+" is hungry.";
+			return getName() + " is hungry.";
 		}	
 	}
 
-	/**feed the cat
-	 * @param p player interacting with cat*/
-	public abstract String feed(Player p);
-	/**play with the cat
-	 * @param p player interacting with cat*/
-	public abstract String play(Player p);
-	/**pet the cat
-	 * @param p player petting cat*/
+	/** Feed the cat
+	 * @param p player interacting with cat 
+    	 */
+   	 public abstract String feed(Player p);
+    
+	/** Play with the cat
+	 * @param p player interacting with cat
+    	 */
+    	public abstract String play(Player p);
+    
+	/** Pet the cat
+	 * @param p player petting cat 
+    	 */
 	public abstract String pet(Player p);
 }
