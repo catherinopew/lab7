81d9ef256d1335f7cbbb11339eabe50c81926f7a
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-07 08:31:32 -0700

    FINAL

--- Tiger.java
+++ Tiger.java
@@ -1,50 +1,56 @@
-/**Class representation for cat type Tiger**/
+/** Class representation for cat type Tiger **/
 public class Tiger extends Cat{
+    /** Initialize Tabby with given name
+	 * @param n name given to Tabby
+	 **/
     public Tiger(String n) {
         super(n);
     }
-    /**Method to feed Tiger
-    *@param p player to feed tiger
-    *@return result from feeding
-    **/
+
+    /** Method to feed Tiger
+     * @param p player to feed tiger
+     * @return result from feeding
+     */
     @Override
     public String feed(Player p) {
         if(getHunger() != 10) {
             incrementHunger(2);
-            return getName() + "is pretty hungry, and starts chomping down on the food right away.";
+            return getName() + " is pretty hungry, and starts chomping down on the food right away.";
         }
         else {
             p.takeDamage(4);
-            return getName() + "is already full and mauls you.";
+            return getName() + " is already full and mauls you.";
         }
     }
-    /**Method to play with tiger
-    *@param p player to play with tiger
-    *@return result from playing with Tiger
-    **/
+
+    /** Method to play with tiger
+     * @param p player to play with tiger
+     * @return result from playing with Tiger
+     */
     @Override
     public String play(Player p) {
         if(getHunger() <= 3) {
             p.takeDamage(4);
-            return getName() + "is pretty hungry and mauls you.";
+            return getName() + " is pretty hungry and mauls you.";
         }
         else {
             incrementHunger(-2);
-            return getName() + "happily plays with you.";
+            return getName() + " happily plays with you.";
         }
     }
-    /**Method to pet tiger
-    *@param p player to pet tiger
-    @return result from petting
-    **/
+
+    /** Method to pet tiger
+     * @param p player to pet tiger
+     * @return result from petting
+     */
     @Override
     public String pet(Player p) {
         if (getHunger() <= 3) {
             p.takeDamage(4);
-            return getName() + "is pretty hungry and mauls you.";
+            return getName() + " is pretty hungry and mauls you.";
         }
         else {
-            return getName() + "happily allows you to pet them.";
+            return getName() + " happily allows you to pet them.";
         }
     }
 }
