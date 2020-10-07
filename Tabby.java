cdfbb117972f5027c37169220c998e4a28ff4630
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-06 20:41:35 -0700

    Create Tabby.java

---
+++ Tabby.java
@@ -0,0 +1,40 @@
+public abstract class Tabby extends Cat{
+	public Tabby(String n){
+        super(n);
+    }
+
+	@Override
+    public String feed(Player p){
+        if(getHunger()!=10){
+            incrementHunger(1);
+            return getName()+" is pretty hungry, and starts chomping down the food right away.";
+        }
+        else{
+            p.takeDamage(2);
+            return getName()+" is already full and mauls you.";
+        }
+    }
+
+	@Override
+	public String play(Player p){
+        if(getHunger()==1){
+            p.takeDamage(2);
+            return getName()+" is too hungry and mauls you.";
+        }
+        else{
+            incrementHunger(-1);
+            return getName()+" happily plays with you.";
+        }
+    }
+
+	@Override
+    public String pet(Player p){
+        if(getHunger()==1){
+            p.takeDamage(2);
+            return getName()+" is too hungry and mauls you.";
+        }
+        else{
+            return getName()+" happily allows you to pet them.";
+        }
+    }
+}
