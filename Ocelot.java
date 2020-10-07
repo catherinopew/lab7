950149746d25dcea61facbaa6c3e4cde07e5abce
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-06 20:53:23 -0700

    final

--- Ocelot.java
+++ Ocelot.java
@@ -1,37 +1,40 @@
 public class Ocelot extends Cat{
 	public Ocelot(String n){
 		super(n);
-	}
+    }
+    
 	@Override
 	public String feed(Player p){
-		if(getHunger()!=10){
+		if(getHunger() != 10){
 			incrementHunger(2);
-			return getName()+" is pretty hungry, and starts chomping down the food right away.";
+			return getName() + " is pretty hungry, and starts chomping down the food right away.";
 		}
 		else{
 			p.takeDamage(3);
-			return getName()+" is already full and mauls you.";
+			return getName() + " is already full and mauls you.";
 		}
-	}
+    }
+    
 	@Override
 	public String play(Player p){
-		if(getHunger()==1){
+		if(getHunger() == 1){
 			p.takeDamage(3);
-			return getName()+" is too hungry and mauls you.";
+			return getName() + " is too hungry and mauls you.";
 		}
 		else{
 			incrementHunger(-1);
-			return getName()+" happily plays with you.";
+			return getName() + " happily plays with you.";
 		}
-	}
+    }
+    
 	@Override
 	public String pet(Player p){
-		if(getHunger()==1){
+		if(getHunger() <= 3){
 			p.takeDamage(3);
-			return getName()+" is too hungry and mauls you.";
+			return getName() + " is too hungry and mauls you.";
 		}
 		else{
-			return getName()+" happily allows you to pet them.";
+			return getName() + " happily allows you to pet them.";
 		}
 	}
 }
