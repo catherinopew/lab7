e5d8030129b76dc3d08c55c90be7f6abb7b53fce
MarvArray <46500443+MarvArray@users.noreply.github.com>
2020-10-06 22:25:16 -0700

    Update Tiger.java
    Finalization

--- Tiger.java
+++ Tiger.java
@@ -1,8 +1,12 @@
+/**Class representation for cat type Tiger**/
 public class Tiger extends Cat{
     public Tiger(String n) {
         super(n);
     }
-
+    /**Method to feed Tiger
+    *@param p player to feed tiger
+    *@return result from feeding
+    **/
     @Override
     public String feed(Player p) {
         if(getHunger() != 10) {
@@ -14,7 +18,10 @@ public class Tiger extends Cat{
             return getName() + "is already full and mauls you.";
         }
     }
-
+    /**Method to play with tiger
+    *@param p player to play with tiger
+    *@return result from playing with Tiger
+    **/
     @Override
     public String play(Player p) {
         if(getHunger() <= 3) {
@@ -26,7 +33,10 @@ public class Tiger extends Cat{
             return getName() + "happily plays with you.";
         }
     }
-
+    /**Method to pet tiger
+    *@param p player to pet tiger
+    @return result from petting
+    **/
     @Override
     public String pet(Player p) {
         if (getHunger() <= 3) {
