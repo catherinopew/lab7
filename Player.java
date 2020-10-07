ebb45ff290f71cfb4ade9f2c717267dc4af1e026
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-06 22:18:15 -0700

    javadocs

--- Player.java
+++ Player.java
@@ -1,18 +1,27 @@
+/**Player to interact in game*/
 public class Player {
+    /**Player's health point value*/
     private int hp;
-
+    
+    /**Initializes player with full 10 health points*/
     public Player() {
         hp = 10;
     }
-
+    /**return player's health points
+    *@return player health points
+    **/
     public int getHp() {
         return hp;
     }
-
+    /**player takes damage from their pet
+    *@param d damge amount
+    **/
     public void takeDamage(int d) {
         hp -= d;
     }
-
+    /**shows if player is dead
+    *@return true if player dies. False otherwise
+    **/
     public boolean isDead() {
         boolean dead = false;
         if (hp <= 0) {
@@ -20,8 +29,10 @@ public class Player {
         }
         return dead;
     }
-
+    /**Print player's HP level
+    *@return player's HP level
+    **/
     public String toString() {
-        return "You have " + getHp() + "/10 HP";
+        return "You have " + getHp() + "/" + hp + " HP";
     }
 }
