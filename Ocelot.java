3dc87268b26821397e48c762555b674c4d979220
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-06 22:09:28 -0700

    javadocs

--- Ocelot.java
+++ Ocelot.java
@@ -1,40 +1,53 @@
+/**represents cat type Ocelot*/
 public class Ocelot extends Cat{
+	/**initializes Ocelot with given name
+	 * @param n ocelot's name*/
 	public Ocelot(String n){
 		super(n);
-    }
-    
+	}
+	
+	/**feeds the ocelot
+	 * @param p player feeding the ocelot
+	 * @return feeding result*/
 	@Override
 	public String feed(Player p){
-		if(getHunger() != 10){
+		if(getHunger()!=10){
 			incrementHunger(2);
-			return getName() + " is pretty hungry, and starts chomping down the food right away.";
+			return getName()+" is pretty hungry, and starts chomping down the food right away.";
 		}
 		else{
 			p.takeDamage(3);
-			return getName() + " is already full and mauls you.";
+			return getName()+" is already full and mauls you.";
 		}
-    }
-    
+	}
+	
+	/**play with the ocelot (do not do this at home)
+	 *@param p player playing with ocelot
+	 *@return play result*/
 	@Override
 	public String play(Player p){
-		if(getHunger() <= 3){
+		if(getHunger()==1){
 			p.takeDamage(3);
-			return getName() + " is too hungry and mauls you.";
+			return getName()+" is too hungry and mauls you.";
 		}
 		else{
 			incrementHunger(-1);
-			return getName() + " happily plays with you.";
+			return getName()+" happily plays with you.";
 		}
-    }
-    
+	}
+
+	/**pet the ocelot
+	 * @param p player interacting with ocelot
+	 * @return pet result
+	 * */
 	@Override
 	public String pet(Player p){
-		if(getHunger() <= 3){
+		if(getHunger()==1){
 			p.takeDamage(3);
-			return getName() + " is too hungry and mauls you.";
+			return getName()+" is too hungry and mauls you.";
 		}
 		else{
-			return getName() + " happily allows you to pet them.";
+			return getName()+" happily allows you to pet them.";
 		}
 	}
 }
