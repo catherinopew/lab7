3442b83be6ccd3f4f8a9c3206d8debbe2b68ce46
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-06 21:31:36 -0700

    javadocs

--- Tabby.java
+++ Tabby.java
@@ -1,40 +1,56 @@
-public class Tabby extends Cat{
+/**represents cat type Tabby*/
+public abstract class Tabby extends Cat{
+	/**initializes Tabby with given name
+	 * @param n tabby's name
+	 * */
 	public Tabby(String n){
-        super(n);
-    }
+                super(n);
+        }
 
+	/**Feed the tabby
+	 * @param p player to interact with Tabby
+	 * @return result of feeding
+	 * */
 	@Override
-    public String feed(Player p){
-        if(getHunger() != 10){
-            incrementHunger(1);
-            return getName() + " is pretty hungry, and starts chomping down the food right away.";
+        public String feed(Player p){
+                if(getHunger()!=10){
+                        incrementHunger(1);
+                        return getName()+" is pretty hungry, and starts chomping down the food right away.";
+                }
+                else{
+                        p.takeDamage(2);
+                        return getName()+" is already full and mauls you.";
+                }
         }
-        else{
-            p.takeDamage(2);
-            return getName() + " is already full and scratches you.";
-        }
-    }
 
+	/**Play with the Tabby
+	 * @param p player to interact with Tabby
+	 * @return result from play time
+	 * */
 	@Override
 	public String play(Player p){
-        if(getHunger() <= 3){
-            p.takeDamage(2);
-            return getName() + " is too hungry and scratches you.";
+                if(getHunger()==1){
+                        p.takeDamage(2);
+                        return getName()+" is too hungry and mauls you.";
+                }
+                else{
+                        incrementHunger(-1);
+                        return getName()+" happily plays with you.";
+                }
         }
-        else{
-            incrementHunger(-1);
-            return getName() + " happily plays with you.";
-        }
-    }
 
+	/**Pet the Tabby
+	 * @param p player to pet Tabby
+	 * @return result from petting Tabby
+	 * */
 	@Override
-    public String pet(Player p){
-        if(getHunger() <= 3){
-            p.takeDamage(2);
-            return getName() + " is too hungry and scratches you.";
+        public String pet(Player p){
+                if(getHunger()==1){
+                        p.takeDamage(2);
+                        return getName()+" is too hungry and mauls you.";
+                }
+                else{
+                        return getName()+" happily allows you to pet them.";
+                }
         }
-        else{
-            return getName() + " happily allows you to pet them.";
-        }
-    }
 }
