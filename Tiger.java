926e39583f93095b87a146da0b88326cb78d431b
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-06 20:42:07 -0700

    Create Tiger.java

---
+++ Tiger.java
@@ -0,0 +1,37 @@
+public class Tiger extends Cat{
+    public Tiger(String n) {
+        super(n);
+    }
+
+    public String feed(Player p) {
+        if(getHunger() != 10) {
+            incrementHunger(2);
+            return getName() + "is pretty hungry, and starts chomping down on the food right away.";
+        }
+        else {
+            p.takeDamage(4);
+            return getName() + "is already full and mauls you.";
+        }
+    }
+
+    public String play(Player p) {
+        if(getHunger() == 1) {
+            p.takeDamage(4);
+            return getName() + "is pretty hungry and mauls you.";
+        }
+        else {
+            incrementHunger(-2);
+            return getName() + "happily plays with you.";
+        }
+    }
+
+    public String pet(Player p) {
+        if (getHunger() == 1) {
+            p.takeDamage(4);
+            return getName() + "is pretty hungry and mauls you.";
+        }
+        else {
+            return getName() + "happily allows you to pet them.";
+        }
+    }
+}
